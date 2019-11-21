982f17f3ae0fa734435f4f9df69776b215b4a94c
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-21 13:55:52 -0800

    bigO for add_first and get_first

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,14 +7,14 @@ class LinkedList
     @head = nil
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
     @head = Node.new(data, @head)
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
     return nil if @head.nil?
     return @head.data
