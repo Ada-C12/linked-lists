8c79b21e62e8a796988846f1d3e36a40dcb7ba9d
Eve Le <nguyenthao.lee@gmail.com>
2019-11-23 14:15:03 -0800

    Implemented add_first and get_first methods for LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,44 +2,47 @@ require_relative 'node'
 
 class LinkedList
   attr_reader :head
-
+  
   def initialize
     @head = nil
   end
-
-  # Time complexity - ?
-  # Space complexity - ?
+  
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    @head = Node.new(data, head)
   end
-
-  # Time complexity - ?
-  # Space complexity - ?
+  
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    if head
+      return @head.data 
+    end
+    return nil
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def length
     return 0
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
-
+    
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def get_last
-
+    
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    
   end
 end
