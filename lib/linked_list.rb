526953ab47becbbab14ffee9ce7938634aa49f4f
Eve Le <nguyenthao.lee@gmail.com>
2019-11-23 14:18:32 -0800

    Implemented add_last and get_last methods for LinkedList and cleaned up code

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,9 +3,11 @@ require_relative 'node'
 class LinkedList
   attr_reader :head
   attr_reader :size
+  attr_reader :tail
   
   def initialize
     @head = nil
+    @tail = nil
     @size = 0
   end
   
@@ -13,15 +15,15 @@ class LinkedList
   # Space complexity - O(1)
   def add_first(data)
     @head = Node.new(data, head)
+    @tail = head  if !tail
+    
     @size +=1
   end
   
   # Time complexity - O(1)
   # Space complexity - O(1)
   def get_first
-    if head
-      return @head.data 
-    end
+    return @head.data if head
     return nil
   end
   
@@ -31,16 +33,23 @@ class LinkedList
     return size
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_last(data)
+    new_node = Node.new(data)
     
+    @tail.next = new_node if tail    
+    @head = new_node if !head
+    
+    @tail = new_node
+    @size += 1
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_last
-    
+    return tail.data if tail
+    return nil
   end
   
   # Time complexity - ?
