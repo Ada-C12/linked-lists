fc936911de1fe283a74d0d90723e47dd3702cf1e
Ga-Young Jin <gyjinn@gmail.com>
2019-11-24 20:25:21 -0800

    completed add/get first and length methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,44 +2,63 @@ require_relative 'node'
 
 class LinkedList
   attr_reader :head
-
+  
   def initialize
     @head = nil
   end
-
-  # Time complexity - ?
-  # Space complexity - ?
+  
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    @head = Node.new(data, head)
   end
-
-  # Time complexity - ?
-  # Space complexity - ?
+  
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return nil if head.nil?
+    return @head.data
   end
-
-  # Time complexity - ?
-  # Space complexity - ?
+  
+  # Time complexity - O(n) where n is the length of the list
+  # Space complexity - O(1)
   def length
-    return 0
+    length_counter = 0
+    current = head
+    until current.nil?
+      current = current.next
+      length_counter += 1
+    end
+    return length_counter
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
-
+    current = head
+    if current.nil?
+      return data
+    end
+    
+    until current.next.nil?
+      current = current.next
+    end
+    current.next = Node.new(data, nil)
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def get_last
-
+    current = head
+    until current.nil?
+      current = current.next
+    end
+    return current.data
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    
   end
 end
