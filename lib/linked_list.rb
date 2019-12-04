203efd0c815a56898bcf6941ecab5757a60d2fbf
Natalie Tapias <nataliemtapias@gmail.com>
2019-12-04 14:26:27 -0800

    fixing length"

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,19 +10,31 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    @head = Node.new(data, head)
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    return nil if @head.nil?
+    return @head.data
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def length
-    return 0
+    counter = 0
+    current = @head
+    if current.nil? 
+      return 0
+    end
+
+    while current != nil
+      current = current.next
+      counter += 1
+    end
+
+    return counter
   end
 
   # Time complexity - ?
@@ -34,12 +46,16 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def get_last
-
+    # until nextNode = nil
+    # end
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    index.times do 
+      self.next
+    end
+    return self.value
   end
 end
