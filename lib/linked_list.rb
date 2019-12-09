217ef89be587f3092bc4d6a1eb1a8a39340bf0cd
Diana <dianah46@gmail.com>
2019-12-08 21:36:39 -0800

    passing all tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,39 +7,78 @@ class LinkedList
     @head = nil
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def add_first(data)
-
+    @head = Node.new(data, head)
+    # current = head
+    # head = new_node
+    # new_node.next_node = current
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
-
+    return nil if @head.nil?
+    return @head.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    return 0 if @head == nil
+
+    length = 0
+    current = @head
+
+    until current.nil?
+      length += 1
+      current = current.next
+    end
+
+    return length
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
+    current = @head
 
+    if current.nil?
+      @head = Node.new(data, head)
+    else
+      until current.next.nil?
+        current = current.next
+      end
+    
+      current.next = Node.new(data)
+    end
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
-
+    current = @head
+    return nil if @head.nil?
+    
+    until current.next.nil?
+      current = current.next
+    end
+    
+    return current.data
   end
 
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
+    return nil if @head.nil?
 
+    current = @head
+
+    index.times do
+      current = current.next
+    end
+
+    return current.data
   end
 end
