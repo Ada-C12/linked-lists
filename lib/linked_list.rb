45fe420cd5ae76be91586fa7d3c7319e03e57bb4
brilatimer <brihiramine@gmail.com>
2019-12-08 16:47:53 -0800

    addfirst, getfirst passing tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,44 +2,48 @@ require_relative 'node'
 
 class LinkedList
   attr_reader :head
-
+  
   def initialize
     @head = nil
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def add_first(data)
-
+    node = Node.new(data, nextNode = @head)
+    @head = node 
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def get_first
-
+    if @head == nil
+      return nil
+    end
+    return @head.data
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def length
     return 0
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
-
+    
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def get_last
-
+    
   end
-
+  
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    
   end
 end
