2022c9226ba561a851b10093729ca4e747510220
Morgan Schuler <morganlschuler@gmail.com>
2019-12-08 22:26:14 -0800

    at last and get at index

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -13,8 +13,8 @@ class LinkedList
     @head = Node.new(data, @head)
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(1)
+  # Space complexity - O(1)
   def get_first
     if @head == nil
       return nil
@@ -23,27 +23,54 @@ class LinkedList
     end
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def length
-    return 0
+    current = @head
+    length = 0
+    until current == nil 
+      length += 1
+      current = current.next
+    end
+    return length
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def add_last(data)
-    
+    if @head == nil
+      @head = Node.new(data, nil)
+    else
+      current = @head
+      until current.next == nil
+        current = current.next
+      end
+      current.next = Node.new(data, nil)
+    end
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_last
-    
+    current = head
+    until current.next == nil
+      current = current.next
+    end
+    return current.data
   end
   
-  # Time complexity - ?
-  # Space complexity - ?
+  # Time complexity - O(n)
+  # Space complexity - O(1)
   def get_at_index(index)
-    
+    current = head
+    count = 0
+    if current == nil
+      return nil
+    end
+    until count == index
+      count += 1
+      current = current.next
+    end
+    return current.data    
   end
 end
